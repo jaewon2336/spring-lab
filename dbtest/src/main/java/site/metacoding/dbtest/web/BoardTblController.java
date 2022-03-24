144a607a110773d289a02500ba31801d073daa96
jaewon2336 <xldzjqpf1588@naver.com>
2022-03-24 10:27:42 +0900

    검색 기능 완료

--- dbtest/src/main/java/site/metacoding/dbtest/web/BoardTblController.java
+++ dbtest/src/main/java/site/metacoding/dbtest/web/BoardTblController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.dbtest.domain.boardTbl.BoardTbl;
@@ -17,9 +18,9 @@ public class BoardTblController {
     private final BoardTblRepository boardTblRepository;
 
     @GetMapping("/search")
-    public String search(Model model) {
+    public String search(Model model, @RequestParam(defaultValue = "") String keyword) { // keyword=스프링
 
-        List<BoardTbl> boards = boardTblRepository.findAll();
+        List<BoardTbl> boards = boardTblRepository.mSearch(keyword);
         model.addAttribute("boards", boards);
 
         return "home";
