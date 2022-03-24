44f8351dae783375ecb5c07cae3951a3ef9b5ac3
jaewon2336 <xldzjqpf1588@naver.com>
2022-03-24 09:55:57 +0900

    home 페이지에 boardTbl 전체목록 출력하기

---
+++ dbtest/src/main/java/site/metacoding/dbtest/web/BoardTblController.java
@@ -0,0 +1,27 @@
+package site.metacoding.dbtest.web;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.dbtest.domain.boardTbl.BoardTbl;
+import site.metacoding.dbtest.domain.boardTbl.BoardTblRepository;
+
+@RequiredArgsConstructor
+@Controller
+public class BoardTblController {
+
+    private final BoardTblRepository boardTblRepository;
+
+    @GetMapping("/search")
+    public String search(Model model) {
+
+        List<BoardTbl> boards = boardTblRepository.findAll();
+        model.addAttribute("boards", boards);
+
+        return "home";
+    }
+}
